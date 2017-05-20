35df79e962b7c2cdb2ac1de1b5541c7e0aa9de80
rekire <git@reki.re>
2017-05-20 17:46:40 +0200

    Add support for cards and remove phoneme tags which seems to be not supported on Google devices

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function convertToAlexaStyle(json) {
             locale: getAlexaLanguage(json.lang),
             type: 'IntentRequest',
             intent: {
-                name: json.result.action,
+                name: json.result.action || json.result.metadata.intentName,
                 slots: {}
             }
         };
         for(let prop in json.result.parameters) {
             if(json.result.parameters.hasOwnProperty(prop)) {
-                json.request.slots[prop] = {name: prop, value: json.result.parameters[prop]}
+                json.request.intent.slots[prop] = {name: prop, value: json.result.parameters[prop]}
             }
         }
         json['session'] = {
@@ -42,8 +42,28 @@ function convertToAlexaStyle(json) {
 function addApiAiAttributes(context) {
     const orgSucceed = context.succeed;
     context.succeed = function(json) {
-        json.speech = json.response.outputSpeech.ssml;
+        json.speech = json.response.outputSpeech.ssml.replace(/(<\/?phoneme([^>]+)>)/ig, "");
         json.displayText = json.speech.replace(/(<([^>]+)>)/ig, "").trim();
+        json.messages = [];
+        if(json.response.card) {
+            const card = json.response.card;
+            json.messages.push({
+                "type": "simple_response",
+                "platform": "google",
+                "textToSpeech": json.speech.replace(/(<([^>]+)>)/ig, "").trim()
+            });
+            switch(card.type) {
+            case 'Standard':
+                json.messages.push({
+                    "type": "basic_card",
+                    "platform": "google",
+                    "title": card.title.replace(/(<([^>]+)>)/ig, ""),
+                    "formattedText": card.text.replace(/(<([^>]+)>)/ig, ""),
+                    "image": {"url": card.image.largeImageUrl},
+                    "buttons": []
+                })
+            }
+        }
         return orgSucceed(json);
     };
     return context;
