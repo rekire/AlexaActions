987bc37bf9344d699b7d9efe5ed70c6d53d7a2db
rekire <git@reki.re>
2017-10-16 19:35:51 +0200

    Try to support contexts

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 function getAlexaLanguage(language) {
     switch(language) {
-    case 'de':
-        return 'de-DE';
-    default:
-        return 'en-US';
+        case 'de':
+            return 'de-DE';
+        default:
+            return 'en-US';
     }
 }
 
 function convertToAlexaStyle(json) {
-    //console.log('input:', json);
+    console.log('input:', json);
     if(json.result && json.result.metadata && json.result.metadata.webhookUsed === 'true') {
-        json['request'] = {
+        json.request = {
             locale: getAlexaLanguage(json.lang),
             type: 'IntentRequest',
             intent: {
@@ -25,15 +25,20 @@ function convertToAlexaStyle(json) {
                 json.request.intent.slots[prop] = {name: prop, value: json.result.parameters[prop]}
             }
         }
-        json['session'] = {
-            'application': {
-                'applicationId': 'some-random-id'
+        json.session = {
+            application: {
+                applicationId: 'some-random-id'
             },
-            'user': {
-                'userId': 'foo'
+            user: {
+                userId: 'foo'
             }
         };
         json.sessionId = 'api.ai.' + json.sessionId;
+
+        json.attributes = json.attributes || {};
+        for(let i = 0; i < json.result.contexts.length; i++) {
+            json.attributes[json.result.contexts[i].name] = json.result.contexts[i].parameters;
+        }
     }
 
     return json;
@@ -42,15 +47,22 @@ function convertToAlexaStyle(json) {
 function addApiAiAttributes(context) {
     const orgSucceed = context.succeed;
     context.succeed = function(json) {
-        json.speech = json.response.outputSpeech.ssml.replace(/(<\/?phoneme([^>]*)>)/ig, "");
-        json.displayText = json.speech.replace(/(<([^>]+)>)/ig, "").trim();
-        if(!json.messages) {
-            json.messages = [];
-            if(json.response.card) {
-                json.messages.push(createDefaultCard(json.speech));
-                json.messages.push(autoConvertCards(json.response.card));
+        if(json.response) {
+            json.speech = json.response.outputSpeech.ssml.replace(/(<\/?phoneme([^>]*)>)/ig, "");
+            json.displayText = json.speech.replace(/(<([^>]+)>)/ig, "").trim();
+            if(!json.messages) {
+                json.messages = [];
+                if(json.response.card) {
+                    json.messages.push(createDefaultCard(json.speech));
+                    json.messages.push(autoConvertCards(json.response.card));
+                }
             }
         }
+        json.contextOut = [];
+        for(let key in json.sessionAttributes) {
+            json.contextOut.push({name: key, lifespan: 60, parameters: json.sessionAttributes[key]});
+        }
+
         console.log("Send out:", json);
         return orgSucceed(json);
     };
@@ -71,17 +83,17 @@ function createDefaultCard(ssml) {
 
 function autoConvertCards(card) {
     switch(card.type) {
-    case 'Standard':
-        return {
-            "type": "basic_card",
-            "platform": "google",
-            "title": card.title.replace(/(<([^>]+)>)/ig, ""),
-            "formattedText": card.text.replace(/(<([^>]+)>)/ig, ""),
-            "image": {"url": card.image.largeImageUrl},
-            "buttons": []
-        };
-    default:
-        throw 'Card type "' + card.type + '" is not yet supported.';
+        case 'Standard':
+            return {
+                "type": "basic_card",
+                "platform": "google",
+                "title": card.title.replace(/(<([^>]+)>)/ig, ""),
+                "formattedText": card.text.replace(/(<([^>]+)>)/ig, ""),
+                "image": {"url": card.image.largeImageUrl},
+                "buttons": []
+            };
+        default:
+            throw 'Card type "' + card.type + '" is not yet supported.';
     }
 }
 
